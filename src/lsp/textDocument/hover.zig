1c7d436401bf7e2a1dcf5857df3894fb6d3c7a61
jmattaa <mattajonathan1@gmail.com>
2025-06-13 13:12:27 +0200

    add documentation comments to the hover

--- src/lsp/textDocument/hover.zig
+++ src/lsp/textDocument/hover.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
-const utils = @import("../../utils.zig");
+
 const State = @import("../../state.zig").State;
+const utils = @import("../../utils.zig");
+
 const quickbuildls = @cImport({
     @cInclude("quickbuildls.h");
 });
@@ -112,11 +114,22 @@ pub fn get_hover_md(
         for (0..s.nfields) |i| {
             const f = s.fields[i];
             if (std.mem.eql(u8, std.mem.span(f.name), ident)) {
-                return try std.fmt.allocPrint(
-                    allocator,
-                    "### Variable: `{s}`",
-                    .{std.mem.span(f.name)},
-                );
+                const doc = try get_doc_cmt(allocator, src, @intCast(f.offset));
+                defer if (doc) |d| allocator.free(d);
+
+                var res = std.ArrayList(u8).init(allocator);
+                defer res.deinit();
+
+                if (doc) |d| {
+                    try res.appendSlice(d);
+                    try res.appendSlice("\n---\n");
+                }
+
+                try res.appendSlice("## Field: `");
+                try res.appendSlice(std.mem.span(f.name));
+                try res.appendSlice("`");
+
+                return try res.toOwnedSlice();
             }
         }
 
@@ -133,31 +146,79 @@ pub fn get_hover_md(
                 if (std.mem.eql(u8, std.mem.span(f.name), ident)) {
                     return try std.fmt.allocPrint(
                         allocator,
-                        "### Field: `{s}`",
+                        "## Field: `{s}`",
                         .{std.mem.span(f.name)},
                     );
                 }
             }
 
             if (std.mem.eql(u8, std.mem.span(t.name), ident)) {
-                if (dependencies) |deps| {
-                    return try std.fmt.allocPrint(
-                        allocator,
-                        "### Task: `{s}`\n---\n#### Depends on: `{s}`",
-                        .{
-                            std.mem.span(t.name), deps,
-                        },
-                    );
-                } else {
-                    return try std.fmt.allocPrint(
-                        allocator,
-                        "### Task: `{s}`",
-                        .{std.mem.span(t.name)},
-                    );
+                const doc = try get_doc_cmt(allocator, src, @intCast(t.offset));
+                defer if (doc) |d| allocator.free(d);
+
+                var res = std.ArrayList(u8).init(allocator);
+                defer res.deinit();
+
+                if (doc) |d| {
+                    try res.appendSlice(d);
+                    try res.appendSlice("\n---\n");
                 }
+
+                try res.appendSlice("## Task: `");
+                try res.appendSlice(std.mem.span(t.name));
+                try res.appendSlice("`");
+
+                if (dependencies) |deps| {
+                    try res.appendSlice("\n### Depends on: `");
+                    try res.appendSlice(deps);
+                    try res.appendSlice("`");
+                }
+
+                return try res.toOwnedSlice();
             }
         }
     }
 
     return null;
+}
+
+fn get_doc_cmt(
+    allocator: std.mem.Allocator,
+    src: []const u8,
+    off: usize,
+) !?[]const u8 {
+    var cmts = std.ArrayList([]const u8).init(allocator);
+    defer cmts.deinit();
+
+    // go to the line above
+    var line_start = off;
+    while (line_start > 0 and src[line_start - 1] != '\n') : (line_start -= 1) {}
+    if (line_start == 0) return null;
+
+    var i = line_start - 1;
+
+    while (i > 0) : (i -= 1) {
+        const lend = i;
+        var lstart = i;
+        while (lstart > 0 and src[lstart - 1] != '\n') : (lstart -= 1) {}
+        const l = src[lstart..lend];
+
+        // skip whitespace
+        var j: usize = 0;
+
+        while (j < l.len and (l[j] == ' ' or l[j] == '\t')) : (j += 1) {}
+        if (j >= l.len or l[j] != '#') break;
+        j += 1; // skip the '#'
+        // skip whitespace
+        while (j < l.len and (l[j] == ' ' or l[j] == '\t')) : (j += 1) {}
+        if (j >= l.len) break;
+
+        try cmts.insert(0, l[j..]);
+
+        if (lstart == 0) break;
+        i = lstart; 
+    }
+
+    if (cmts.items.len == 0) return null;
+    return try std.mem.join(allocator, "\n", cmts.items);
 }
